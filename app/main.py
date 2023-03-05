7d9708f55050a85a3ba08bcbec9e24c1a76b3294
tructt <tieuthu.anninh2@gmail.com>
2023-03-05 10:34:23 +0700

    update cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
 from .routers import posts, users, auth, votes
@@ -6,6 +7,17 @@ from .routers import posts, users, auth, votes
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
+origins = ["https://www.google.com",
+           "http://localhost:8000",
+           "http://127.0.0.1:8000",
+           ]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(auth.router)
